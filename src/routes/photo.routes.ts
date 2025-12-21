206146c94ec6f2bfc934677fe48a0f84c8bfc638
Rzkykhrllh <rizkykirigaya@gmail.com>
2025-12-21 15:30:00 +0900

    Create UpdatePhoto and DeletePhoto  API

--- src/routes/photo.routes.ts
+++ src/routes/photo.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { getPhotos, getPhotoById, createPhoto } from "../controllers/photo.controller";
+import { getPhotos, getPhotoById, createPhoto, updatePhoto, deletePhoto } from "../controllers/photo.controller";
 import { authenticateToken } from "../middlewares/auth.middleware";
 
 
 const router = Router();
 
 // Public Routes
-router.get("/", getPhotos)
+router.get("/", getPhotos);
 router.get("/:id", getPhotoById);
 
 // Protected Routes
 router.post("/",authenticateToken ,createPhoto);
+router.put("/:id", authenticateToken, updatePhoto); 
+router.delete("/:id", authenticateToken, deletePhoto);
 
 export default router;
